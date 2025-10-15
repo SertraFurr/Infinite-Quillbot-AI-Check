f40238bf7b41ddbc9ed744c435db0ee80ca16135
Sertra <Amaryllisaea@gmail.com>
2025-10-15 15:20:58 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+localStorage.setItem('aidr_daily_scan_count', -9999999);
+
+// NOT MANDATORY IF YOU WISH TO CHECK IF THE VALUE IS GOOD OR NOT !
+console.log('aidr_daily_scan_count =', localStorage.getItem('aidr_daily_scan_count'));
